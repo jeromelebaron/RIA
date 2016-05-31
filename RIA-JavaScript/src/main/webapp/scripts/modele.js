cf2337c4c60dfa67dee809eef167b2efdf8c90b5
Jerdopler <lebaronjerome@free.fr>
2016-05-31 16:20:27 +0200

    Avancement du cours

---
+++ RIA-JavaScript/src/main/webapp/scripts/modele.js
@@ -0,0 +1,11 @@
+var laListe = ['ga', 'zo','meu'];
+
+function ajouterALaListe (argument) {
+	laListe.push(argument);
+}
+
+function supprimerDeLaListe (index) {
+	if (typeof laListe[index] != 'undefined') {
+		laListe.splice(index, 1);
+	}
+}
