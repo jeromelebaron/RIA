d9fc7c654ab2bcd8351e949c0e1dc41126e5910c
Jerdopler <lebaronjerome@free.fr>
2016-06-03 09:37:32 +0200

    Ajout de AngularJS

---
+++ RIA-AngularJS/src/main/webapp/scripts/app.js
@@ -0,0 +1,6 @@
+/**
+ * Pour déclarer un module Angular.
+ * [monApp description]
+ * @type {[type]}
+ */
+var monApp = angular.module('monApp', []);
--- RIA-JavaScript/src/main/webapp/scripts/chienVuejquery.js
+++ RIA-JavaScript/src/main/webapp/scripts/chienVuejquery.js
@@ -1,62 +1,89 @@
 var chienVue = (function() {
 
-	function render() {
-		creerTableauChien(controlerChien.getChiens());
-	};
+    /**
+     * Fonction appelée pour créer le tableau de chiens.
+     */
+    function render() {
+        creerTableauChien(controlerChien.getChiens());
+    };
 
-	function creerTableauChien(listeChien) {
-		var corpsTableau = $('#contenuTableau');
-		$.each(listeChien, function(index, element) {
-			var nouvelleLigne = $('<tr>');
-			nouvelleLigne.click(controlerChien.afficherDetail.bind(null, element.id));
-			nouvelleLigne.append(creerCellule(element.id));
-			corpsTableau.append(nouvelleLigne);
-			nouvelleLigne.append(creerCellule(element.nom));
-			corpsTableau.append(nouvelleLigne);
-			nouvelleLigne.append(creerCellule(element.race));
-			corpsTableau.append(nouvelleLigne);
-			nouvelleLigne.append(creerCellule(element.age));
-			corpsTableau.append(nouvelleLigne);
-			nouvelleLigne.append(creerCellule(element.lof));
-			corpsTableau.append(nouvelleLigne);
-		});
-	};
+    /**
+     * Pour construire le tableau avec les chiens.
+     * @param  {une liste de chien} la liste des chiens.
+     * @return {[type]}
+     */
+    function creerTableauChien(listeChien) {
+        var corpsTableau = $('#contenuTableau');
+        $.each(listeChien, function(index, element) {
+            var nouvelleLigne = $('<tr>');
+            nouvelleLigne.click(controlerChien.afficherDetail.bind(null, element.id));
+            nouvelleLigne.append(creerCellule(element.id));
+            corpsTableau.append(nouvelleLigne);
+            nouvelleLigne.append(creerCellule(element.nom));
+            corpsTableau.append(nouvelleLigne);
+            nouvelleLigne.append(creerCellule(element.race));
+            corpsTableau.append(nouvelleLigne);
+            nouvelleLigne.append(creerCellule(element.age));
+            corpsTableau.append(nouvelleLigne);
+            nouvelleLigne.append(creerCellule(element.lof));
+            corpsTableau.append(nouvelleLigne);
+        });
+    };
 
-	function creerCellule(element) {
-		var cellule = $('<td>');
-		cellule.text(element);
-		return cellule;
-	};
+    /**
+     * Pour créer une cellule du tableau.
+     * @param  {String} l'élement à ajouter dans le tableau.
+     * @return {[type]}
+     */
+    function creerCellule(element) {
+        var cellule = $('<td>');
+        cellule.text(element);
+        return cellule;
+    };
 
-	var ul;
+    var ul;
 
-	function detailChien() {
-		$('#titre').text(controlerChien.getChien().nom);
-		var detailChien = $('#detailChien');
-		if (ul !== undefined) {
-			ul.remove();
-		};
-		detailChien.append(construireDetailChien());
-		$("#myModal").modal();
-	};
+    /**
+     * Pour afficher le détail d'un chien.
+     * @return {function}
+     */
+    function detailChien() {
+        $('#titre').text(controlerChien.getChien().nom);
+        var detailChien = $('#detailChien');
+        if (ul !== undefined) {
+            ul.remove();
+        };
+        detailChien.append(construireDetailChien());
+        $("#myModal").modal();
+    };
 
-	function construireDetailChien() {
-		ul = $('<ul>');
-		ul.append(contruireElementDetailChien('Race', controlerChien.getChien().race));
-		ul.append(contruireElementDetailChien('Age', controlerChien.getChien().age));
-		ul.append(contruireElementDetailChien('Est inscrit au lof', controlerChien.getChien().lof));
-		return ul;
-	};
+    /**
+     * Pour fabriquer la liste de détails du chien.
+     * @return {ul}
+     */
+    function construireDetailChien() {
+        ul = $('<ul>');
+        ul.append(contruireElementDetailChien('Race', controlerChien.getChien().race));
+        ul.append(contruireElementDetailChien('Age', controlerChien.getChien().age));
+        ul.append(contruireElementDetailChien('Est inscrit au lof', controlerChien.getChien().lof));
+        return ul;
+    };
 
-	function contruireElementDetailChien(type, element) {
-		var li = $('<li>');
-		li.text(type + ' : ' + element);
-		return li;
-	};
+    /**
+     * Pour ajouer une puce à la liste de détails du chien.
+     * @param  {String} le libellé du détail.
+     * @param  {String} le détail.
+     * @return {function}
+     */
+    function contruireElementDetailChien(type, element) {
+        var li = $('<li>');
+        li.text(type + ' : ' + element);
+        return li;
+    };
 
-	return {
-		render: render,
-		detailChien: detailChien
-	};
+    return {
+        render: render,
+        detailChien: detailChien
+    };
 
 })();
