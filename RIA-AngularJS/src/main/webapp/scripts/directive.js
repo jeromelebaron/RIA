1f174297e57c35e755c20d1ab42d5dc19dfe9ee5
Jerdopler <lebaronjerome@free.fr>
2016-06-06 14:00:25 +0200

    Fin du cours angularsJS

--- RIA-AngularJS/src/main/webapp/scripts/directive.js
+++ RIA-AngularJS/src/main/webapp/scripts/directive.js
@@ -33,9 +33,9 @@ monApp.directive('afcTable', [function() {
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
 		// template: '',
-		templateUrl: 'templates/afctable.html'
-			// replace: true,
-			// transclude: true,
+		templateUrl: 'templates/afctable.html',
+		// replace: true,
+		transclude: true
 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 	};
 }]);
